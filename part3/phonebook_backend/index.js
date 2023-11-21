6c3b232e9839267ad12df45054bd87e172e5149f
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-11-21 16:34:44 -0600

    Completed exercise 3.5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -59,6 +59,32 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    // const maxId = persons.length > 0
+    //     ? Math.max(...persons.map(n => n.id))
+    //     : 0
+    return Math.floor(Math.random() * 10000)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    // if (!body.content) {
+    //     return response.status(400).json({
+    //         error: 'content missing'
+    //     })
+    // }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
