77b31c6cd444343aa1d38e18b568baa7e2832dd1
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-11-26 14:25:15 -0600

    Finished adding morgan middleware, exercise 3.7

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 
+var morgan = require('morgan')
+
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
 app.use(express.json())
+// app.use(requestLogger)
+app.use(morgan('tiny'))
 
 let persons = [
     { 
