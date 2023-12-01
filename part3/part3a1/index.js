c1998550825bddf59d144f3735361ec94b2e3c98
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-12-01 16:39:24 -0600

    Added models folder with mongodb connection to backend

--- part3/part3a1/index.js
+++ part3/part3a1/index.js
@@ -28,25 +28,34 @@
 // const PORT = 3001
 // app.listen(PORT)
 // console.log(`Server running on port ${PORT}`)
+
+// const mongoose = require('mongoose')
+// const mongodbPassword = process.env.VITE_MONGODB_PASSWORD
+
+// const url = `mongodb+srv://epalma7482:${mongodbPassword}@cluster0.sh7mhbj.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+// mongoose.set('strictQuery', false)
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//     content: String,
+//     important: Boolean,
+// })
+
+// noteSchema.set('toJSON', {
+//     transform: (document, returnedObject) => {
+//         returnedObject.id = returnedObject._id.toString()
+//         delete returnedObject._id
+//         delete returnedObject.__v
+//     }
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const mongodbPassword = process.env.VITE_MONGODB_PASSWORD
-
-const url = `mongodb+srv://epalma7482:${mongodbPassword}@cluster0.sh7mhbj.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -57,9 +66,11 @@ const requestLogger = (request, response, next) => {
   }
 
 app.use(express.json())
-app.use(requestLogger)
+// app.use(requestLogger)
 app.use(cors())
 app.use(express.static('dist'))
+
+
 
 let notes = [
     {
@@ -150,7 +161,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
