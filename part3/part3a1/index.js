0d5c1a88e7bddc3bd0606328d247a750e7a87eba
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-12-08 15:53:13 -0600

    Restructured backend, modules for routes, middleware,config, and logger

--- part3/part3a1/index.js
+++ part3/part3a1/index.js
@@ -1,190 +1,131 @@
-// const http = require('http')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only JavaScript",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         important: true
-//     }
-// ]
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
 
 
-// const app = http.createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'application/json'})
-//     response.end(JSON.stringify(notes))
+
+// // require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const config = require('./utils/config')
+// const logger = require('./utils/logger')
+// const Note = require('./models/note')
+
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method)
+//     console.log('Path:  ', request.path)
+//     console.log('Body:  ', request.body)
+//     console.log('---')
+//     next()
+// }
+
+// app.use(express.json())
+// app.use(cors())
+// app.use(express.static('dist'))
+// app.use(requestLogger)
+
+// app.listen(config.PORT, () => {
+//     logger.info(`Server running on port ${config.PORT}`)
 // })
 
-
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server running on port ${PORT}`)
-
-// const mongoose = require('mongoose')
-// const mongodbPassword = process.env.VITE_MONGODB_PASSWORD
-
-// const url = `mongodb+srv://epalma7482:${mongodbPassword}@cluster0.sh7mhbj.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-// mongoose.set('strictQuery', false)
-// mongoose.connect(url)
-
-// const noteSchema = new mongoose.Schema({
-//     content: String,
-//     important: Boolean,
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World! Test</h1>')
 // })
 
-// noteSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         returnedObject.id = returnedObject._id.toString()
-//         delete returnedObject._id
-//         delete returnedObject.__v
-//     }
+// app.get('/api/notes', (request, response, next) => {
+//   Note.find({})
+//     .then(notes => {
+//         response.json(notes)
+//     })
+//     .catch(error => next(error))
 // })
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only JavaScript",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         important: true
-//     },
-//     {
-//         id: 4,
-//         content: "testing",
-//         important: true
-//     }
-// ]
-// const Note = mongoose.model('Note', noteSchema)
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Note = require('./models/note')
-// const note = require('./models/note')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
+// app.get('/api/notes/:id', (request, response, next) => {
+//     Note.findById(request.params.id)
+//         .then(note => {
+//             if (note) {
+//                 response.json(note) 
+//             } else {
+//                 response.status(404).end()
+//             }
+//         })
+//         .catch(error => next(error))
+// })
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-app.use(requestLogger)
+// app.put('/api/notes/:id', (request, response, next) => {
+//     const {content, important} = request.body
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World! Test</h1>')
-})
+//     Note.findByIdAndUpdate(
+//         request.params.id,
+//         {content, important},
+//         {new: true, runValidators: true, context: 'query'}
+//     )
+//     .then(updatedNote => {
+//         response.json(updatedNote)
+//     })
+//     .catch(error => next(error))
+// })
 
-app.get('/api/notes', (request, response, next) => {
-  Note.find({})
-    .then(notes => {
-        response.json(notes)
-    })
-    .catch(error => next(error))
-})
+// app.delete('/api/notes/:id', (request, response, next) => {
+//     Note.findByIdAndDelete(request.params.id)
+//         .then(result => {
+//             response.status(204).end()
+//         })
+//         .catch(error => next(error))
+// })
 
-app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note) 
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//         ? Math.max(...notes.map(n => n.id))
+//         : 0
+//     return maxId + 1
+// }
 
-app.put('/api/notes/:id', (request, response, next) => {
-    const {content, important} = request.body
-
-    Note.findByIdAndUpdate(
-        request.params.id,
-        {content, important},
-        {new: true, runValidators: true, context: 'query'}
-    )
-    .then(updatedNote => {
-        response.json(updatedNote)
-    })
-    .catch(error => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+// app.post('/api/notes', (request, response, next) => {
+//     const body = request.body
     
-    if (body.content === undefined) {
-        return response.status(400).json({error: 'content missing'})
-    }
+//     if (body.content === undefined) {
+//         return response.status(400).json({error: 'content missing'})
+//     }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
+//     const note = new Note({
+//         content: body.content,
+//         important: body.important || false,
+//     })
 
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
-})
+//     note.save()
+//         .then(savedNote => {
+//             response.json(savedNote)
+//         })
+//         .catch(error => next(error))
+// })
 
-//handler of requests with unknown endpoint
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+// //handler of requests with unknown endpoint
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint' })
+// }
+// app.use(unknownEndpoint)
 
-//handler of requests with result to errors
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+// //handler of requests with result to errors
+// const errorHandler = (error, request, response, next) => {
+//     console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
-    next(error)
-}
-app.use(errorHandler)
+//     if (error.name === 'CastError') {
+//         return response.status(400).send({error: 'malformatted id'})
+//     } else if (error.name === 'ValidationError') {
+//         return response.status(400).json({error: error.message})
+//     }
+//     next(error)
+// }
+// app.use(errorHandler)
 
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// const PORT = process.env.PORT
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
