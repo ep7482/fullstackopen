97a8d0015d25788d054e65de3f5e8750a33753e6
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-12-13 14:30:54 -0600

    Completed exercise 4.9, added test helper and test cases

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./helper')
 const app = require('../app')
-
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+	await Blog.deleteMany({})
+
+	const blogObjects = helper.initialBlogs
+		.map(blog => new Blog(blog))
+	const promiseArray = blogObjects.map(blog => blog.save())
+	await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
 	await api
@@ -15,7 +24,15 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
 	const response = await api.get('/api/blogs')
 
-	expect(response.body).toHaveLength(3)
+	expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('unique identifier is defined', async () => {
+	const blogs = await helper.blogsInDb()
+
+	blogs.forEach(blog => {
+		expect(blog.id).toBeDefined()
+	})
 })
 
 afterAll(async () => {
