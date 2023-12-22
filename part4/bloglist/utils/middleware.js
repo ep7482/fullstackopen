430cf1576b1b5a1ebddb360910c162ee17565523
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-12-21 19:18:09 -0600

    Completed exercise 4.20, refactor getToken to middleware

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -12,6 +12,14 @@ const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.startsWith('Bearer ')) {
+		request.token = authorization.replace('Bearer ', '')
+	}
+	next()
+}
+
 const errorHandler = (error, request, response, next) => {
 	logger.error(error.message)
 
@@ -28,8 +36,11 @@ const errorHandler = (error, request, response, next) => {
 	next(error)
 }
 
+
+
 module.exports = {
 	requestLogger,
 	unknownEndpoint,
-	errorHandler
+	errorHandler,
+	tokenExtractor
 }
