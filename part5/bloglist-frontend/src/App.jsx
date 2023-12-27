1950a2ba571c240b191e3e6a349c9c9d1e1661c3
ep7482 <47162290+ep7482@users.noreply.github.com>
2023-12-27 16:40:32 -0600

    Completed exercise 5.10, sorted blog post by likes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -126,7 +126,7 @@ const App = () => {
       <Notification message={errorMessage} type='success'/>
       <p>{user.name} logged in</p>
       {blogForm()}
-      {blogs.map(blog =>
+      {blogs.sort((a,b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike}/>
       )}
       <button onClick={handleLogout}>logout</button>
