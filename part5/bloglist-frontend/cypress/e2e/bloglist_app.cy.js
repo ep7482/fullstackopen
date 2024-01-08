0c4d7fb9608c9c18f060a07c8b85359a1824e432
ep7482 <47162290+ep7482@users.noreply.github.com>
2024-01-07 23:00:15 -0600

    Completed exercise 5.19, added logged in user testcases

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe('Bloglist App', () => {
   beforeEach(() => {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'testpassword'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
   it('front page can be opened', () => {
@@ -10,5 +16,22 @@ describe('Bloglist App', () => {
 
   it('login form can be opened', () => {
     cy.contains('login').click()
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.contains('login').click()
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+      cy.contains('Test User logged in')
+    })
+    it('fails with wrong credentials', () => {
+      cy.contains('login').click()
+      cy.get('#username').type('wrongtestuser')
+      cy.get('#password').type('wrongtestpassword')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+    })
   })
 })
