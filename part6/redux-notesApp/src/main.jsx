67d1b2740f342aa5fe36300ad8aae0450a7ce940
ep7482 <47162290+ep7482@users.noreply.github.com>
2024-01-10 23:44:32 -0600

    Created new noteapp with redux state management

---
+++ part6/redux-notesApp/src/main.jsx
@@ -0,0 +1,57 @@
+/* esling-disable */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+import { createStore } from 'redux'
+
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    return state.concat(action.payload)
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+})
+
+const App = () => {
+  return(
+    <div>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>)}
+      </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+StorageEvent.subscribe(renderApp)
