d90d20b571982f5cadb26a591d0f845591eb6c5c
ep7482 <47162290+ep7482@users.noreply.github.com>
2024-01-12 22:31:52 -0600

    Completed exercise 6.6-7, added action creator functions to reducer file and create AddedForm with creation of new anecdote logic into component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,21 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, voteAct } from './reducers/anecdoteReducer'
+import { voteAct } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
   const vote = (id) => dispatch(voteAct(id))
-
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
-
-  
 
   return (
     <div>
@@ -31,11 +22,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote'/></div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteForm/>
     </div>
   )
 }
