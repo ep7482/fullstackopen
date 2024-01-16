bd7265b77735fb9c21d307fac8c377a633590a68
ep7482 <47162290+ep7482@users.noreply.github.com>
2024-01-15 21:31:53 -0600

    Followed part 6c, communicating with server

--- part6/redux-notesApp/src/main.jsx
+++ part6/redux-notesApp/src/main.jsx
@@ -2,15 +2,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+import noteService from './services/notes'
 
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+})
 
+// noteService.getAll().then(notes =>
+//   store.dispatch(setNotes(notes))
+// )
+
+// console.log(store.getState())
+// store.subscribe(() => console.log(store.getState()))
+// store.dispatch(filterChange('ALL'))
+// store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-notesApp/src/reducers/noteReducer.js
+++ part6/redux-notesApp/src/reducers/noteReducer.js
@@ -1,44 +1,92 @@
-const noteReducer = (state = [], action) => {
-  switch(action.type) {
-		case 'NEW_NOTE':
-			return [...state, action.payload]
-		case 'TOGGLE_IMPORTANCE': {
-			const id = action.payload.id
+import { createSlice } from '@reduxjs/toolkit'
+
+// const initialState = [
+// 	{
+// 		content: 'reducer defines how redux store works',
+// 		important: true,
+// 		id: 1,
+// 	},
+// 	{
+// 		content: 'state of store can contain any data',
+// 		important: false,
+// 		id: 2,
+// 	}
+// ]
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+const noteSlice = createSlice({
+	name: 'notes',
+	initialState: [],
+	reducers: {
+		createNote(state, action) {
+			state.push(action.payload)
+		},
+		toggleImportanceOf(state, action) {
+			const id = action.payload
 			const noteToChange = state.find(n => n.id === id)
 			const changedNote = {
 				...noteToChange,
 				important: !noteToChange.important
 			}
+			console.log(JSON.parse(JSON.stringify(state)))
 			return state.map(note =>
 				note.id !== id ? note : changedNote
 			)
+		},
+		appendNote(state, action) {
+			state.push(action.payload)
+		},
+		setNotes(state, action) {
+			return action.payload
 		}
-		default:
-			return state
 	}
-}
+})
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export default noteSlice.reducer
 
-export const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
+// const noteReducer = (state = initialState, action) => {
+//   switch(action.type) {
+// 		case 'NEW_NOTE':
+// 			return [...state, action.payload]
+// 		case 'TOGGLE_IMPORTANCE': {
+// 			const id = action.payload.id
+// 			const noteToChange = state.find(n => n.id === id)
+// 			const changedNote = {
+// 				...noteToChange,
+// 				important: !noteToChange.important
+// 			}
+// 			return state.map(note =>
+// 				note.id !== id ? note : changedNote
+// 			)
+// 		}
+// 		default:
+// 			return state
+// 	}
+// }
 
-export const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id }
-  }
-}
 
-export default noteReducer
+
+// export const createNote = (content) => {
+//   return {
+//     type: 'NEW_NOTE',
+//     payload: {
+//       content,
+//       important: false,
+//       id: generateId()
+//     }
+//   }
+// }
+
+// export const toggleImportanceOf = (id) => {
+//   return {
+//     type: 'TOGGLE_IMPORTANCE',
+//     payload: { id }
+//   }
+// }
+
+// export default noteReducer
 
 // const store = createStore(noteReducer)
 
